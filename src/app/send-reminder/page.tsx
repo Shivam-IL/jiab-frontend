456fef41ce1784f528873828170054d73a63447d
Ishaan Mittal <ishaan.mittal@infinitelocus.com>
2025-05-30 12:06:31 +0530

    Fix : Add toast in reminder

--- src/app/send-reminder/page.tsx
+++ src/app/send-reminder/page.tsx
@@ -6,10 +6,12 @@ import ScreenWrapper from '@/components/common/ScreenWrapper'
 import SvgIcons from '@/components/common/SvgIcons'
 import { ICONS_NAMES, MOBILE_TEMP_NAVBAR_DATA, STATUS, USER } from '@/constants'
 import useWindowWidth from '@/hooks/useWindowWidth'
-import React from 'react'
+import React, { useState } from 'react'
 
 const SendReminderPage = () => {
   const width = useWindowWidth()
+
+  const [sendReminder, setSendReminder] = useState<boolean>(false)
   return (
     <div className='flex flex-col gap-3'>
       <MobileTempNavBar title={MOBILE_TEMP_NAVBAR_DATA.SEND_REMINDER.TITLE} />
@@ -17,7 +19,11 @@ const SendReminderPage = () => {
         className={`${width > 750 ? 'mt-[71px] flex justify-center' : 'mt-0'}`}
       >
         <div className='relative w-full md:flex hidden justify-center items-center md:mb-[24px]'>
-          <AktivGroteskText text='Send Reminder' fontSize='text-[30px]' fontWeight='font-[700]' />
+          <AktivGroteskText
+            text='Send Reminder'
+            fontSize='text-[30px]'
+            fontWeight='font-[700]'
+          />
         </div>
         <div className='flex flex-col gap-[16px] md:gap-[24px]'>
           <div className='flex justify-between px-[12px] py-[12px] md:px-[45px] md:py-[22px] bg-[#FFE200] rounded-[5px] md:rounded-[20px]'>
@@ -45,7 +51,15 @@ const SendReminderPage = () => {
                   fontWeight='font-[400]'
                 />
               </div>
-              <button className='flex flex-col items-center justify-center'>
+              <button
+                onClick={() => {
+                  setSendReminder(true)
+                  setTimeout(() => {
+                    setSendReminder(false)
+                  }, 1000)
+                }}
+                className='flex flex-col items-center justify-center'
+              >
                 <AktivGroteskText
                   className='text-[#1985D3]'
                   text='Send Again'
@@ -57,6 +71,18 @@ const SendReminderPage = () => {
           </div>
         </div>
       </ScreenWrapper>
+      {sendReminder && (
+        <div className='fixed bottom-[10%] w-[100%]  flex justify-center items-center'>
+          <div className='rounded-[10px] bg-[#009233] w-[90%] md:w-[50%]'>
+            <AktivGroteskText
+              className='self-center py-[8px] text-white text-center'
+              text='You can only remind your friend three times!'
+              fontSize='text-[12px] md:text-[16px]'
+              fontWeight='font-[400]'
+            />
+          </div>
+        </div>
+      )}
     </div>
   )
 }
