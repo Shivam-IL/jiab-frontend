3f9a96f04d758703ac0ad66ed5b07f2b12b2b114
Ishaan Mittal <ishaan.mittal@infinitelocus.com>
2025-06-15 20:34:56 +0530

    Fix : Signup

--- src/components/common/ContentButton/index.tsx
+++ src/components/common/ContentButton/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { IContentButton } from "@/interfaces";
 import Image from "next/image";
-const ContentButton: React.FC<IContentButton> = ({ text, onClick, icon, className }) => {
+const ContentButton: React.FC<IContentButton> = ({ text, onClick, icon, className, id }) => {
   return (
     <button
+      id={id}
       onClick={onClick}
       className={`md:text-[32px] text-[12px] rounded-full flex items-center md:gap-[16px] gap-[8.06px] justify-center md:font-bold font-medium ${className}`}
     >
