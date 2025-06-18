0843c4308f4155628d86cfab97273ef944df6079
S4TAN <satviksharma@Satviks-MacBook-Pro.local>
2025-06-18 13:05:28 +0530

    Done

--- src/components/HomePageClient.tsx
+++ src/components/HomePageClient.tsx
@@ -342,7 +342,7 @@ export default function HomePageClient () {
     <div className='bg-lightGray min-h-screen'>
       {/* SerialChillerPopup only once after login */}
 
-      <div className='md:w-[76.57%] mx-auto md:pt-24 pt-20'>
+      <div className="md:w-[76.57%] mx-auto md:pt-24 pt-20">
         {/* Modals */}
         {!isAuthenticated &&
           refreshTokenNotVerified &&
