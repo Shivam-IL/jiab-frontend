b24499c04294d5c650d77c2a0b5d85b7edd82709
S4TAN <satviksharma@Satviks-MacBook-Pro.local>
2025-06-21 11:31:14 +0530

    Done

--- src/components/common/CustomPopupWrapper/index.tsx
+++ src/components/common/CustomPopupWrapper/index.tsx
@@ -1,11 +1,11 @@
-import { IExitPopupWrapper } from '@/interfaces'
-import React from 'react'
-import CustomDialogWrapper from '../CustomDialogWrapper'
-import SvgIcons from '../SvgIcons'
-import AktivGroteskText from '../AktivGroteskText'
-import GreenCTA from '@/components/GreenCTA'
-import { aktivGrotesk } from '@/app/layout'
-import { on } from 'events'
+import { IExitPopupWrapper } from "@/interfaces";
+import React from "react";
+import CustomDialogWrapper from "../CustomDialogWrapper";
+import SvgIcons from "../SvgIcons";
+import AktivGroteskText from "../AktivGroteskText";
+import GreenCTA from "@/components/GreenCTA";
+import { aktivGrotesk } from "@/app/layout";
+import { on } from "events";
 
 const CustomPopupWrapper: React.FC<IExitPopupWrapper> = ({
   open,
@@ -24,91 +24,91 @@ const CustomPopupWrapper: React.FC<IExitPopupWrapper> = ({
   yesButtonClick,
   noButtonClick,
   yesButtonText,
-  noButtonText
+  noButtonText,
 }) => {
   return (
     <CustomDialogWrapper
       open={open}
       onClose={() => {
         if (onClose) {
-          onClose()
+          onClose();
         }
       }}
     >
-      <div className='w-full  h-full flex flex-col gap-[16px] items-center justify-center'>
-        <div className='flex flex-col items-center justify-center w-[50px] h-[50px] rounded-full bg-[#FFE200]'>
-          <SvgIcons name={icon} className='w-[25px] h-[25px]' />
+      <div className="w-full  h-full flex flex-col gap-[16px] items-center justify-center">
+        <div className="flex flex-col items-center justify-center w-[50px] h-[50px] rounded-full bg-[#FFE200]">
+          <SvgIcons name={icon} className="w-[25px] h-[25px]" />
         </div>
-        <div className='flex flex-col text-center items-center px-[27px] w-[90%] justify-center gap-[16px]'>
+        <div className="flex flex-col text-center items-center px-[27px] w-[90%] justify-center gap-[16px]">
           <AktivGroteskText
             text={title}
-            fontSize='text-[18px]'
-            fontWeight='font-[700]'
+            fontSize="text-[18px]"
+            fontWeight="font-[700]"
           />
           <AktivGroteskText
             text={subtitle}
-            fontSize='text-[16px]'
-            fontWeight='font-[400]'
+            fontSize="text-[16px]"
+            fontWeight="font-[400]"
           />
           {doubleButton && (
             <AktivGroteskText
-              text={sureToExitText || 'Are you sure you want to exit?'}
-              fontSize='text-[14px]'
-              fontWeight='font-[500]'
+              text={sureToExitText || "Are you sure you want to exit?"}
+              fontSize="text-[14px]"
+              fontWeight="font-[500]"
             />
           )}
         </div>
         {doubleButton && (
-          <div className='flex pb-[16px] gap-[14px]'>
+          <div className="flex pb-[16px] gap-[14px]">
             <button
               onClick={() => {
                 if (onClose) {
-                  onClose()
+                  onClose();
                 }
                 if (yesButtonClick) {
-                  yesButtonClick()
+                  yesButtonClick();
                 }
               }}
               className={`py-[10px] ${aktivGrotesk.className} leading-tight px-[28px] bg-white border-[1px] border-black rounded-[100px] text-[14px] md:text-[18px] font-[700]`}
             >
-              {yesButtonText || 'Yes'}
+              {yesButtonText || "Yes"}
             </button>
             <GreenCTA
-              className='leading-tight'
-              fontSize='text-[14px] md:text-[18px]'
-              paddingClass='py-[10px] px-[28px]'
-              text={noButtonText || 'No'}
+              className="leading-tight"
+              fontSize="text-[14px] md:text-[18px]"
+              paddingClass="py-[10px] px-[28px]"
+              text={noButtonText || "No"}
               onClick={() => {
                 if (setOpen) {
-                  setOpen(false)
+                  setOpen(false);
                 }
                 if (noButtonClick) {
-                  noButtonClick()
+                  noButtonClick();
                 }
               }}
             />
           </div>
         )}
-        {childrenPosition === 'top' && (
-          <div className='w-[80%] self-center flex flex-col'>{children}</div>
+        {childrenPosition === "top" && (
+          <div className="w-[80%] self-center flex flex-col">{children}</div>
         )}
         {singleButton && (
-          <div className='flex pb-[16px] gap-[14px]'>
+          <div className="flex pb-[16px] gap-[14px]">
             <GreenCTA
-              className='leading-tight'
-              fontSize='text-[14px] md:text-[18px]'
-              paddingClass='py-[10px] px-[28px]'
-              text={singleButtonText || ''}
+              className="leading-tight"
+              fontSize="text-[14px] md:text-[18px]"
+              paddingClass="py-[10px] px-[28px]"
+              text={singleButtonText || ""}
               onClick={singleButtonOnClick || onClose}
             />
           </div>
         )}
-        {children && childrenPosition !== 'top' && (
-          <div className='pb-[16px] w-[80%] self-center'>{children}</div>
+        {children && childrenPosition !== "top" && (
+          <div className="pb-[16px] w-[80%] self-center">{children}</div>
         )}
       </div>
     </CustomDialogWrapper>
-  )
-}
+  );
+};
 
-export default CustomPopupWrapper
+export default CustomPopupWrapper;
