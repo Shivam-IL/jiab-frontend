21f7938f4527b91a96925195fba331e70736cd8b
S4TAN <satviksharma@Satviks-MacBook-Pro.local>
2025-06-23 00:42:13 +0530

    Done

--- src/components/common/HelpUsToKnowYourBetter/index.tsx
+++ src/components/common/HelpUsToKnowYourBetter/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import AktivGroteskText from "../AktivGroteskText";
 import {
   GA_EVENTS,
