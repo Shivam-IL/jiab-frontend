27c76aff8541fe633a91e1eeaff8d4fb147f3946
Ishaan Mittal <ishaan.mittal@infinitelocus.com>
2025-06-23 17:29:26 +0530

    Fix refer

--- src/components/common/ReferNowComponent/index.tsx
+++ src/components/common/ReferNowComponent/index.tsx
@@ -1,175 +1,179 @@
-import React, { useCallback, useEffect, useState } from "react";
-import CustomPopupWrapper from "../CustomPopupWrapper";
+import React, { useCallback, useEffect, useState } from 'react'
+import CustomPopupWrapper from '../CustomPopupWrapper'
 import {
   GA_EVENTS,
   REFERRAL_CODE,
-  REFFERAL_STATUS_POPUP_DATA,
-} from "@/constants";
-import AktivGroteskText from "../AktivGroteskText";
-import ReferNowModal from "../ReferNowModal";
-import { useSendReferral } from "@/api/hooks/ReferralHooks";
-import { triggerGAEvent } from "@/utils/gTagEvents";
-import { useCMSData } from "@/data";
-import { useSendCDPEvent } from "@/api/hooks/CDPHooks";
-import useAppSelector from "@/hooks/useSelector";
+  REFFERAL_STATUS_POPUP_DATA
+} from '@/constants'
+import AktivGroteskText from '../AktivGroteskText'
+import ReferNowModal from '../ReferNowModal'
+import { useSendReferral } from '@/api/hooks/ReferralHooks'
+import { triggerGAEvent } from '@/utils/gTagEvents'
+import { useCMSData } from '@/data'
+import { useSendCDPEvent } from '@/api/hooks/CDPHooks'
+import useAppSelector from '@/hooks/useSelector'
 import {
   BaseCDPEventPayload,
-  CDPEventPayloadBuilder,
-} from "@/api/utils/cdpEvents";
+  CDPEventPayloadBuilder
+} from '@/api/utils/cdpEvents'
 
 const ReferNowComponent = ({
   open,
   onClose,
   setOpen,
-  setReferralCode,
+  setReferralCode
 }: {
-  open: boolean;
-  onClose: () => void;
-  setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
-  setReferralCode?: React.Dispatch<React.SetStateAction<string>>;
+  open: boolean
+  onClose: () => void
+  setOpen?: React.Dispatch<React.SetStateAction<boolean>>
+  setReferralCode?: React.Dispatch<React.SetStateAction<string>>
 }) => {
-  const [open2, setOpen2] = useState<boolean>(false);
-  const [open3, setOpen3] = useState<boolean>(false);
+  const [open2, setOpen2] = useState<boolean>(false)
+  const [open3, setOpen3] = useState<boolean>(false)
 
-  const [referStatus1, setReferStatus1] = useState<boolean>(false);
-  const [referStatus2, setReferStatus2] = useState<boolean>(false);
-  const [phoneNumber, setPhoneNumber] = useState<string>("");
-  const [error, setError] = useState<string>("");
+  const [referStatus1, setReferStatus1] = useState<boolean>(false)
+  const [referStatus2, setReferStatus2] = useState<boolean>(false)
+  const [phoneNumber, setPhoneNumber] = useState<string>('')
+  const [error, setError] = useState<string>('')
 
-  const [referStatus, setReferStatus] = useState<string>("");
-  const [inviteCode, setInviteCode] = useState<string>("");
+  const [referStatus, setReferStatus] = useState<string>('')
+  const [inviteCode, setInviteCode] = useState<string>('')
 
-  const { mutate: sendReferral, data: sendReferralData } = useSendReferral();
-  const { mutate: sendCDPEvent } = useSendCDPEvent();
-  const { user } = useAppSelector((state) => state.profile);
+  const { mutate: sendReferral, data: sendReferralData } = useSendReferral()
+  const { mutate: sendCDPEvent } = useSendCDPEvent()
+  const { user } = useAppSelector(state => state.profile)
 
   const triggerReferCDPEvent = useCallback(() => {
     if (user?.id) {
       const payload: BaseCDPEventPayload =
         CDPEventPayloadBuilder.buildReferFriendPayload({
-          user_identifier: user.id,
-        });
-      sendCDPEvent(payload);
+          user_identifier: user.id
+        })
+      sendCDPEvent(payload)
     }
-  }, [sendCDPEvent, user?.id]);
+  }, [sendCDPEvent, user?.id])
 
   const handleChange = (key: string, value: string) => {
     if (value?.length === 10 && parseInt(value?.[0]) >= 6) {
       if (open2) {
-        const numericValue = value?.replace(/[^0-9]/g, "");
-        const valueString = numericValue?.slice(0, 10);
-        setPhoneNumber(valueString);
-        setOpen2(false);
-        setOpen?.(true);
-        setError("");
-        return;
+        const numericValue = value?.replace(/[^0-9]/g, '')
+        const valueString = numericValue?.slice(0, 10)
+        setPhoneNumber(valueString)
+        setOpen2(false)
+        setOpen?.(true)
+        setError('')
+        return
       }
     }
 
     if (value?.length === 10 && parseInt(value?.[0]) < 6) {
-      setError("Please enter a valid 10 digit mobile number");
+      setError('Please enter a valid 10 digit mobile number')
     }
 
-    const numericValue = value?.replace(/[^0-9]/g, "");
-    const valueString = numericValue?.slice(0, 10);
-    setPhoneNumber(valueString);
-  };
+    const numericValue = value?.replace(/[^0-9]/g, '')
+    const valueString = numericValue?.slice(0, 10)
+    setPhoneNumber(valueString)
+  }
 
   const submitReferNow = () => {
     if (phoneNumber?.length === 10 && parseInt(phoneNumber?.[0]) < 6) {
-      console.log("here 2");
-      setOpen2(true);
-      setOpen?.(false);
-      setReferStatus(REFERRAL_CODE.INVALID_MOBILE_NUMBER);
-      return;
+      console.log('here 2')
+      setOpen2(true)
+      setOpen?.(false)
+      setReferStatus(REFERRAL_CODE.INVALID_MOBILE_NUMBER)
+      return
     }
 
     if (phoneNumber?.length < 10 && parseInt(phoneNumber?.[0]) < 6) {
-      setError("Please enter a valid 10 digit mobile number");
-      setOpen2(true);
-      setOpen?.(false);
-      setReferStatus(REFERRAL_CODE.INVALID_MOBILE_NUMBER);
-      return;
+      setError('Please enter a valid 10 digit mobile number')
+      setOpen2(true)
+      setOpen?.(false)
+      setReferStatus(REFERRAL_CODE.INVALID_MOBILE_NUMBER)
+      return
     }
 
     sendReferral({
-      refer_to: phoneNumber,
-    });
-  };
+      refer_to: phoneNumber
+    })
+  }
 
   useEffect(() => {
     if (sendReferralData?.ok) {
       const { status } = sendReferralData?.data as {
-        status: string;
-        invite_code?: string;
-      };
-      triggerReferCDPEvent();
-      setReferStatus(status);
+        status: string
+        invite_code?: string
+      }
+      triggerReferCDPEvent()
+      setReferStatus(status)
       if (status === REFERRAL_CODE.SUCCESS) {
-        setReferStatus1(true);
-        setOpen2(false);
-        setOpen3(false);
+        setReferStatus1(true)
+        setOpen2(false)
+        setOpen3(false)
 
         setInviteCode(
           (sendReferralData?.data as unknown as { invite_code: string })
-            ?.invite_code ?? ""
-        );
+            ?.invite_code ?? ''
+        )
         if (setReferralCode) {
           setReferralCode(
             (sendReferralData?.data as unknown as { invite_code: string })
-              ?.invite_code ?? ""
-          );
+              ?.invite_code ?? ''
+          )
         }
-        onClose();
+        onClose()
       } else if (status === REFERRAL_CODE.ALREADY_REFERRED) {
-        setReferStatus2(true);
-        setOpen2(false);
-        setOpen3(false);
-        setReferStatus1(false);
+        setReferStatus2(true)
+        setOpen2(false)
+        setOpen3(false)
+        setReferStatus1(false)
 
-        onClose();
+        onClose()
       } else if (status === REFERRAL_CODE.CANNOT_SEND_TO_SELF) {
-        setOpen3(true);
-        setOpen2(false);
+        setOpen3(true)
+        setOpen2(false)
 
-        setReferStatus1(false);
-        setReferStatus2(false);
-        onClose();
+        setReferStatus1(false)
+        setReferStatus2(false)
+        onClose()
       } else if (status === REFERRAL_CODE.INVALID_MOBILE_NUMBER) {
-        setOpen2(true);
-        setOpen3(false);
-        setReferStatus1(false);
-        setReferStatus2(false);
-        onClose();
+        setOpen2(true)
+        setOpen3(false)
+        setReferStatus1(false)
+        setReferStatus2(false)
+        onClose()
       } else if (status === REFERRAL_CODE.EXISTING_USER) {
-        setOpen?.(true);
-        setOpen3(false);
-        setReferStatus1(false);
-        setReferStatus2(false);
-        setOpen2?.(true);
-        setError("Cannot refer an existing user");
+        setOpen?.(true)
+        setOpen3(false)
+        setReferStatus1(false)
+        setReferStatus2(false)
+        setOpen2?.(true)
+        setError('Cannot refer an existing user')
       }
     }
-  }, [
-    sendReferralData,
-    triggerReferCDPEvent,
-    setReferStatus,
-    setReferStatus1,
-    setReferStatus2,
-    setOpen2,
-    setOpen3,
-    setInviteCode,
-    setReferralCode,
-    onClose,
-  ]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sendReferralData])
 
   useEffect(() => {
     return () => {
-      setPhoneNumber("");
-    };
-  }, []);
+      setOpen?.(false)
+      setOpen2(false)
+      setOpen3(false)
+      setReferStatus1(false)
+      setReferStatus2(false)
+      setPhoneNumber('')
+      setError('')
+      setInviteCode('')
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-  const { easyPeasy, ahemAhem, tryingToPrankUs, referAFriend } = useCMSData();
+  useEffect(() => {
+    return () => {
+      setPhoneNumber('')
+    }
+  }, [])
+
+  const { easyPeasy, ahemAhem, tryingToPrankUs, referAFriend } = useCMSData()
 
   return (
     <>
@@ -183,13 +187,13 @@ const ReferNowComponent = ({
           onChange={handleChange}
           open={open}
           onSubmit={() => {
-            triggerGAEvent(GA_EVENTS.SPRITE_J24_REFER_NOW);
-            submitReferNow();
+            triggerGAEvent(GA_EVENTS.SPRITE_J24_REFER_NOW)
+            submitReferNow()
           }}
           error={error}
           onClose={() => {
-            onClose();
-            setPhoneNumber("");
+            onClose()
+            setPhoneNumber('')
           }}
         />
       )}
@@ -203,13 +207,13 @@ const ReferNowComponent = ({
           onChange={handleChange}
           error={error}
           onSubmit={() => {
-            submitReferNow();
+            submitReferNow()
           }}
           open={open2}
           onClose={() => {
-            onClose();
-            setPhoneNumber("");
-            setOpen2(false);
+            onClose()
+            setPhoneNumber('')
+            setOpen2(false)
           }}
         />
       )}
@@ -221,13 +225,13 @@ const ReferNowComponent = ({
           phoneNumber={phoneNumber}
           placeholder={referAFriend.mobileNumberTextField}
           onSubmit={() => {
-            submitReferNow();
+            submitReferNow()
           }}
           onChange={handleChange}
           open={open3}
           onClose={() => {
-            setOpen3(false);
-            setPhoneNumber("");
+            setOpen3(false)
+            setPhoneNumber('')
           }}
         />
       )}
@@ -235,23 +239,23 @@ const ReferNowComponent = ({
         <CustomPopupWrapper
           open={referStatus1}
           onClose={() => {
-            setReferStatus1(false);
-            setPhoneNumber("");
+            setReferStatus1(false)
+            setPhoneNumber('')
           }}
           icon={REFFERAL_STATUS_POPUP_DATA.EASY.ICON}
           title={easyPeasy.easy_me_pop_up_heading}
           subtitle={easyPeasy.easy_me_pop_up_sub_heading_1}
         >
-          <div className="flex flex-col gap-[20px]">
+          <div className='flex flex-col gap-[20px]'>
             <AktivGroteskText
-              fontSize="text-[16px]"
-              fontWeight="font-[700]"
-              className="text-[#00953B] text-center"
+              fontSize='text-[16px]'
+              fontWeight='font-[700]'
+              className='text-[#00953B] text-center'
               text={`"${inviteCode}"`}
             />
             <AktivGroteskText
-              fontSize="text-[12px]"
-              fontWeight="font-[400] text-center"
+              fontSize='text-[12px]'
+              fontWeight='font-[400] text-center'
               text={easyPeasy.easy_me_pop_up_sub_heading}
             />
           </div>
@@ -261,8 +265,8 @@ const ReferNowComponent = ({
         <CustomPopupWrapper
           open={referStatus2}
           onClose={() => {
-            setReferStatus2(false);
-            setPhoneNumber("");
+            setReferStatus2(false)
+            setPhoneNumber('')
           }}
           icon={REFFERAL_STATUS_POPUP_DATA.PAST_ON_US.ICON}
           title={REFFERAL_STATUS_POPUP_DATA.PAST_ON_US.TITLE}
@@ -272,13 +276,13 @@ const ReferNowComponent = ({
             REFFERAL_STATUS_POPUP_DATA.PAST_ON_US.SINGLE_BUTTON_TEXT
           }
           singleButtonOnClick={() => {
-            setReferStatus2(false);
-            setOpen?.(true);
+            setReferStatus2(false)
+            setOpen?.(true)
           }}
         />
       )}
     </>
-  );
-};
+  )
+}
 
-export default ReferNowComponent;
+export default ReferNowComponent
